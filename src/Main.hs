b04e9f156ce0080ecd548e1b14dfb3b25983f9aa
Fraser Tweedale <frase@frase.id.au>
2023-12-01 13:08:10 +1000

    main: better errors for out-of-bounds year or day

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+import Control.Monad (when)
 import System.Environment (getArgs, getProgName)
 import System.Exit (die)
 import Text.Read (readMaybe)
@@ -18,7 +19,14 @@ main = do
   (y,d) <- case args of
     [y,d] -> maybe usage pure $ (,) <$> readMaybe y <*> readMaybe d
     _ -> usage
-  years !! (y - firstYear) !! (d - 1)
+  let i'y = y - firstYear
+  when (i'y < 0 || i'y >= length years) $
+    die $ "unknown year: " <> show y
+  let days = years !! i'y
+      i'd = d - 1
+  when (i'd < 0 || i'd >= length days) $
+    die $ "unknown day: " <> show d
+  days !! i'd
 
 usage :: IO a
 usage = do
