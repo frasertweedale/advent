e255fed4facfbb3c3f161bee1b45adf2294b1a18
Fraser Tweedale <frase@frase.id.au>
2023-12-01 21:02:07 +1000

    2023 day 1 puzzle 1
    Topics: monoid, foldMap

--- src/Y2023/D01.hs
+++ src/Y2023/D01.hs
@@ -1,7 +1,19 @@
 module Y2023.D01 (solutions) where
 
+import Data.Char
+import Data.Monoid
+import Data.Maybe (mapMaybe)
+
 solutions :: [IO ()]
 solutions = [s1]
 
 s1 :: IO ()
-s1 = interact (unlines . fmap reverse . lines)
+s1 = getContents >>= print . go . lines
+  where
+  go = sum . mapMaybe extract
+  extract s = (\hi lo -> hi * 10 + lo) <$> firstDig s <*> lastDig s
+  firstDig = getFirst . foldMap (First . toDigit)
+  lastDig = getLast . foldMap (Last . toDigit)
+  toDigit c
+    | isDigit c = Just (ord c - 0x30)
+    | otherwise = Nothing
