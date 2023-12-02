3794c8d22685cc68227bd12b684d71cc4668826c
Fraser Tweedale <frase@frase.id.au>
2023-12-02 16:54:02 +1000

    2023 day 2 puzzle 2
    Time taken: ~25 minutes
    Topics: semigroup, foldMap
    
    Solution would be cleaner with Foldable1, but that only
    arrived in base-4.18.0.0 (GHC 9.6).

--- src/Y2023/D02.hs
+++ src/Y2023/D02.hs
@@ -3,14 +3,17 @@
 module Y2023.D02 (solutions) where
 
 import Data.Maybe (mapMaybe)
+import Data.Semigroup (Max(..))
 
 import Util.Parser
 
 solutions :: [IO ()]
-solutions = [s1]
+solutions = [s1 gameNumberIfValid, s1 power]
 
-s1 :: IO ()
-s1 = getContents >>= print . sum . mapMaybe play . lines
+type Strategy = Game -> Int
+
+s1 :: Strategy -> IO ()
+s1 strat = getContents >>= print . sum . mapMaybe (play strat) . lines
 
 data Colour = R | G | B
 
@@ -27,13 +30,31 @@ colourCountValid colour n = case colour of
 roundValid :: Round -> Bool
 roundValid = all (uncurry colourCountValid)
 
--- | Parse game string and return game number if game is valid
-play :: String -> Maybe Int
-play s = do
-  (Game n rounds, _s') <- runParser parseGame s
-  if all roundValid rounds
-    then Just n
-    else Nothing
+gameNumberIfValid :: Strategy
+gameNumberIfValid (Game n rounds)
+  | all roundValid rounds = n
+  | otherwise             = 0
+
+power :: Strategy
+power (Game _ rounds) = mult (maxesGame rounds)
+  where
+    mult (Max r, Max g, Max b) = r * g * b
+    {- Note: these would be much nicer with Foldable1 -}
+    maxesGame =
+      foldr (\rnd r -> maxesRound rnd <> r) (Max 0, Max 0, Max 0)
+    maxesRound =
+      foldr (\colourCount r -> maxesColourCount colourCount <> r) (Max 0, Max 0, Max 0)
+    maxesColourCount (colour, n) =
+      case colour of
+        R -> (Max n, Max 0, Max 0)
+        G -> (Max 0, Max n, Max 0)
+        B -> (Max 0, Max 0, Max n)
+
+-- | Parse game string and apply strategy
+play :: Strategy -> String -> Maybe Int
+play strat s = do
+  (game, _s') <- runParser parseGame s
+  pure $ strat game
 
 parseGame :: Parser Game
 parseGame = do
