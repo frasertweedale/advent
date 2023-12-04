81c95df54f12c78f3f479d369ce8219e24a319fd
Fraser Tweedale <frase@frase.id.au>
2023-12-04 22:44:43 +1000

    2023 day 4 puzzle 2
    Time taken: 20 minutes
    Topics: recursion, laziness
    
    Spent a solid 5 minutes debugging a silly mistake.  I had written
    `[0..]` and `[1..]` instead of `repeat 0` and `repeat 1` for
    constructing the occurrence list.

--- src/Y2023/D04.hs
+++ src/Y2023/D04.hs
@@ -1,16 +1,35 @@
 module Y2023.D04 (solutions) where
 
+import Data.Maybe (mapMaybe)
 import Data.List (intersect)
 
 import Util.Parser
 
 solutions :: [IO ()]
-solutions = [s1]
+solutions = [s1, s2]
 
 s1 :: IO ()
 s1 =
   getContents
   >>= print . sum . fmap (maybe 0 score . parseCard) . lines
+
+s2 :: IO ()
+s2 =
+  getContents
+  >>= print . sum . go (repeat (1::Int)) . mapMaybe parseCard . lines
+  where
+  go _ [] = []
+  go [] _ = [] -- can't happen
+  -- n = how many occurrences of THIS card
+  -- occurs = how many occurrences of following cards
+  -- card = current card
+  -- deck = rest of cards
+  go (n:occurs) (card:deck) =
+    let
+      factors' =
+        zipWith (+) (replicate (numMatches card) n <> repeat 0) occurs
+    in
+      n : go factors' deck
 
 score :: Card -> Int
 score game = case numMatches game of
