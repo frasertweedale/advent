6ad0a836005841a261144c2a2a862bc36455d56e
Fraser Tweedale <frase@frase.id.au>
2023-12-08 23:18:58 +1000

    2023 day 8 puzzle 2
    Time taken: too much
    
    Certain invariants that are not disclosed in the puzzle text had to
    be discovered experimentally (or by searching online). With the
    hidden facts, the solution is a straightforward *least common
    multiple* calculation (`lcm`).
    
    Without awareness of these invariants, the naïve algorithm takes an
    impossibly long time to run.  For example, the answer for my puzzle
    input was 18,215,611,419,223.  I ran the naïve solution for > 4
    hours and I don't know how many iterations it did, but it sure as
    heck wasn't that many!
    
    The hidden invariants are:
    
    1. The path from each 'A' node to the first 'Z' node has a number of
       steps that is a multiple of the (uncycled) path length.
    
    2. The path from each of the first 'Z' nodes reaches another 'Z'
       node, and that path segment is the same length as the previous
       segment, and so on.  Incidentally, each path loops back to the
       same 'Z' node, guaranteeing this invariant in the simplest way.
    
    Accordingly, LCM of these path lengths computes the solution.
    
    I really do not like having to discover hidden invariants in the
    input data in order to solve the problem.  This is the first such
    problem (in 2023 anyway) where the problem statement is insufficient
    and I hope it is the last.

--- src/Y2023/D08.hs
+++ src/Y2023/D08.hs
@@ -8,7 +8,7 @@ import Data.Foldable (toList)
 import Util.Parser
 
 solutions :: [IO ()]
-solutions = [s1 go1]
+solutions = [s1 go1, s1 go2]
 
 s1 :: (Show a) => (Input -> a) -> IO ()
 s1 go =
@@ -23,6 +23,57 @@ go1 (path, nodes) = followPath "AAA" (cycle path)
   followPath loc (dir:dirs) = 1 + case lookup loc nodes of
     Nothing -> 0
     Just (l,r) -> followPath (case dir of L -> l ; R -> r) dirs
+
+
+--                      ┌───  A to Z₁ ──┐ ┌──  Z₁ to Z₂ ──┐┌Z₁=Z₂┐
+go2 :: Input -> (Int, [(Int,Bool,Location,Int,Bool,Location,Bool)])
+go2 (path, nodes) = (n, results)
+  where
+    -- This outputs the lengths of the paths from the A node to
+    -- the first Z node; and again to the next Z node, verifies
+    -- that these lengths are a multiple of the path length,
+    -- and verifies that the first Z node and second Z node are the
+    -- same node.
+    f node =
+      let
+        firstCycle = cycleToZ nodes (cycle path) node
+        lenFirstCycle = length $ drop 1 firstCycle
+        (midLoc, midDirs) = last firstCycle
+        secondCycle = cycleToZ nodes midDirs midLoc
+        lenSecondCycle = length $ drop 1 firstCycle
+        (endLoc, _) = last secondCycle
+      in
+        ( lenFirstCycle, lenFirstCycle `mod` length path == 0, midLoc
+        , lenSecondCycle, lenSecondCycle `mod` length path == 0, endLoc
+        , midLoc == endLoc
+        )
+    results = fmap f startNodes
+    n = foldr lcm 1 $ fmap (\(x,_,_,_,_,_,_) -> x) results
+    startNodes = filter (endsWith 'A') $ fmap fst nodes
+
+-- | Return a list of locations reached from the starting
+-- location, terminating when we reach a valid endpoint.
+-- Also return the tail of path from point at which we stop.
+--
+cycleToZ
+  :: [(Location, (Location, Location))]
+  -> Path
+  -> Location
+  -> [(Location, Path)]
+cycleToZ nodeMap path = doit path
+  where
+    doit [] loc           = doit path loc
+    doit (dir:dirs) loc
+      | endsWith 'Z' loc  = (loc,dir:dirs) : []
+      | otherwise         = case lookup loc nodeMap of
+          Nothing ->
+            []  -- shouldn't happen
+          Just (l,r) ->
+            (loc,dir:dirs) : doit dirs (case dir of L -> l ; R -> r)
+
+endsWith :: Char -> String -> Bool
+endsWith c [_,_,c3] = c == c3
+endsWith _ _        = False
 
 
 type Input = (Path, [Node])
