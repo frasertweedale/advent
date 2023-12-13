33d0b967e6e143c386d0ecc9c58cda103fd5a15f
Fraser Tweedale <frase@frase.id.au>
2023-12-13 12:49:35 +1000

    2023 day 12 puzzle 2
    Time taken: lots
    Topics: state machine, combinatorics
    
    Naïve implementations take "heat death of universe" time.  I found
    some optimisations but after running overnight, there were still
    some input lines that had not produced an answer.
    
    The key insight was to realise that divergent paths often "collapse"
    to a common state.  As a minimal example:
    
        .??.??. 1,1
    
    There are 4 possible valid arrangements:
    
        ..#..#.  /  ..#.#..  /  .#...#.  /  .#..#..
    
    When we reach the mid-point, there were two possible arrangements so
    far.  But there is only one "state" of what remains to be done, i.e.
    `.??. 1`.  After each step (consumption of exactly one character
    from the template), we gather all the next possible states and
    propagate each state *and its multiplicity* to the next step (a
    multiset of states).  When we reach the end of the template, the sum
    of the multiplicities of valid end states is the number of possible
    arrangements.

--- src/Y2023/D12.hs
+++ src/Y2023/D12.hs
@@ -1,39 +1,101 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
+
 module Y2023.D12 (solutions) where
 
-import Data.Foldable (toList)
-import Data.List.NonEmpty (group)
+import Data.Foldable
+import Data.List (intercalate)
+import Data.List.NonEmpty (groupWith)
 import qualified Data.List.NonEmpty as NonEmpty
 
 import Util.Parser
 
 solutions :: [IO ()]
-solutions = [s1 go1]
+solutions = [s1 (go1 id), s1 (go1 prep2)]
 
 s1 :: (Show a) => (Input -> a) -> IO ()
 s1 go =
   getContents
   >>= print . go . maybe (error "parse failed") fst . runParser parseInput
 
-go1 :: Input -> Int
-go1 = sum . fmap validArrangements
+go1 :: ((Template,[ClusterSize]) -> (Template,[ClusterSize])) -> Input -> Int
+go1 prep = sum . fmap (uncurry walk . prep)
 
-validArrangements :: (Template, [ClusterSize]) -> Int
-validArrangements (template, sizes) =
-  length $ filter (isValid sizes) $ fillTemplate template
+prep2 :: (Template,[ClusterSize]) -> (Template,[ClusterSize])
+prep2 (template, sizes) =
+  ( intercalate "?" $ replicate 5 template
+  , concat $ replicate 5 sizes
+  )
 
-fillTemplate :: Template -> [Template]
-fillTemplate "" = [""]
-fillTemplate (c:cs) =
-  case c of
-    '?' -> fmap ('#':) (fillTemplate cs) <> fmap ('.':) (fillTemplate cs)
-    _   -> fmap (c:) (fillTemplate cs)
+data PathState
+  -- | Need to consume a '#', or a '?' which we will treat as a '#'
+  = NeedHash Int
+  -- | Need to consume a '.', or a '?' which we will treat as a '.'
+  -- (group just ended, need a gap).
+  | NeedDot
+  -- | Can eat any character.  '#' will start eating a new hash
+  -- group, '.' will just carry on at the next character, and
+  -- '?' will produce both next states.
+  | Don'tCare
+  deriving (Eq, Ord, Show)
 
-isValid :: [ClusterSize] -> Template -> Bool
-isValid sizes =
-  (== sizes)
-  . fmap length
-  . filter ((== '#') . NonEmpty.head)
-  . group
+data Path = Path PathState [ClusterSize]
+  deriving (Eq, Ord, Show)
+
+feedPath :: Char -> Path -> [Path]
+feedPath c = \case
+  Path (NeedHash n) xs -> case c of
+    '.'             -> []
+    _ | n > 1       -> [ Path (NeedHash (n - 1)) xs ]
+    _               -> [ Path NeedDot xs ]
+  Path NeedDot xs -> case c of
+    '#'             -> []
+    _               -> [ Path Don'tCare xs ]
+  Path Don'tCare [] -> case c of
+    '#'             -> []
+    _               -> [ Path Don'tCare [] ]
+  Path Don'tCare (n:xs) -> case c of
+    '#' | n > 1     -> [ Path (NeedHash (n - 1)) xs ]
+        | otherwise -> [ Path NeedDot xs ]
+    '?' | n > 1     -> [ Path Don'tCare (n:xs)
+                       , Path (NeedHash (n - 1)) xs ]
+        | otherwise -> [ Path Don'tCare (n:xs)
+                       , Path NeedDot xs ]
+    _ {- '.' -}     -> [ Path Don'tCare (n:xs) ]
+
+walk :: Template -> [ClusterSize] -> Int
+walk s xs = go s (start xs)
+  where
+    go "" paths =
+      sum . fmap (\(mul, path) -> mul * endPath path) $ paths
+    go (c:cs) paths =
+      go cs (step c paths)
+
+step :: Char -> [(Int, Path)] -> [(Int, Path)]
+step c paths =
+  joinPaths
+  $ paths >>= \(mul, path) -> fmap (mul,) (feedPath c path)
+
+-- | Gather distinct paths, taking the sum of their multiplicands.
+--
+joinPaths :: [(Int, Path)] -> [(Int, Path)]
+joinPaths =
+  fmap (\l -> (sum (fmap fst l), snd (NonEmpty.head l)))
+  . groupWith snd
+
+-- | The pattern has terminated.  Express validity of
+-- the residual Path value as a multiplicand (0 or 1).
+--
+endPath :: Path -> Int
+endPath = \case
+  Path NeedDot []   -> 1
+  Path Don'tCare [] -> 1
+  _                 -> 0
+
+-- Construct start state
+start :: [ClusterSize] -> [(Int, Path)]
+start xs = [ ( 1, Path Don'tCare xs ) ]
+
 
 type Input = [(Template,[ClusterSize])]
 type Template = String
