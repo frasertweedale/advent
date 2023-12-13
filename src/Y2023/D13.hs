94f63e8857df5ef77aafeec5730e3c6051639de8
Fraser Tweedale <frase@frase.id.au>
2023-12-13 16:28:16 +1000

    2023 day 13 puzzle 2
    Time taken: 60 minutes

--- src/Y2023/D13.hs
+++ src/Y2023/D13.hs
@@ -3,30 +3,56 @@ module Y2023.D13 (solutions) where
 import Data.List (elemIndex, transpose)
 
 solutions :: [IO ()]
-solutions = [s1 go1]
+solutions = [s1 (go1 Nothing), s1 (go1 (Just smudge))]
 
 s1 :: (Show a) => ([Pattern] -> a) -> IO ()
 s1 go =
   getContents
   >>= print . go . parseInput
 
-go1 :: [Pattern] -> Int
-go1 = sum . fmap reflSum
+go1 :: Maybe Smudge -> [Pattern] -> Int
+go1 f = sum . fmap (reflSum f)
 
-reflSum :: Pattern -> Int
-reflSum pat = sum (reflVert pat) + 100 * sum (reflHoriz pat)
+reflSum :: Maybe Smudge -> Pattern -> Int
+reflSum f pat = sum (reflVert f pat) + 100 * sum (reflHoriz f pat)
 
-reflVert :: Pattern -> [Int]
-reflVert = reflHoriz . transpose
+-- | Function that returns possible smudges of a line
+type Smudge = String -> [String]
 
-reflHoriz :: Pattern -> [Int]
-reflHoriz = go 0 []
+-- | Smudge a line
+smudge :: Smudge
+smudge ""     = []
+smudge (c:cs) = (opposite c : cs) : fmap (c:) (smudge cs)
+
+opposite :: Char -> Char
+opposite = \case
+  '.' -> '#'
+  '#' -> '.'
+  c   -> c
+
+
+reflVert :: Maybe Smudge -> Pattern -> [Int]
+reflVert f = reflHoriz f . transpose
+
+reflHoriz :: Maybe Smudge -> Pattern -> [Int]
+reflHoriz f = go 0 []
   where
   go n [] (x:xs) = go (n + 1) [x] xs
   go _ _  [] = []
   go n l r@(rh:rt)
-    | all (uncurry (==)) (zip l r)  = [n]
-    | otherwise                     = go (n + 1) (rh:l) rt
+    | isRefl f (zip l r)  = [n]
+    | otherwise           = go (n + 1) (rh:l) rt
+
+-- Smudge, if given, MUST be applied.
+--
+isRefl :: Maybe Smudge -> [(String, String)] -> Bool
+isRefl mf = go mf
+  where
+  go f []                                 = maybe True (const $ null f) mf
+  go f ((l,r):more)
+    | l == r                              = isRefl f more
+    | maybe False (any (== r) . ($ l)) f  = isRefl Nothing more
+    | otherwise                           = False
 
 type Pattern = [String]
 
