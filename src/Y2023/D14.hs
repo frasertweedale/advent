e8babcadcd2732154c7529970c11c16640f1a3b9
Fraser Tweedale <frase@frase.id.au>
2023-12-14 18:04:38 +1000

    2023 day 14 puzzle 2
    Time taken: 60 minutes
    Topics: list, cycle detection
    
    I lost a lot of time debugging because I mixed up East and West >_<

--- src/Y2023/D14.hs
+++ src/Y2023/D14.hs
@@ -2,8 +2,10 @@ module Y2023.D14 (solutions) where
 
 import Data.List (sort, transpose)
 
+import qualified Util.Map as Map
+
 solutions :: [IO ()]
-solutions = [s1 go1]
+solutions = [s1 go1, s1 go2]
 
 s1 :: (Show a) => (Pattern -> a) -> IO ()
 s1 go =
@@ -11,9 +13,43 @@ s1 go =
   >>= print . go . lines
 
 go1 :: Pattern -> Int
-go1 = sum . fmap loadLeft . fmap tiltLeft . transpose
+go1 = sum . extractLoads . fmap tiltLeft . orient
+
+go2 :: Pattern -> Int
+go2 = sum . extractLoads . go Map.empty . zip [0..] . every4th . tilts . orient
+  where
+  go _ [] = error "can't happen"
+  go seen ((i, pat):more) = case Map.lookup pat seen of
+    Nothing -> go (Map.insert pat i seen) more
+    Just i0 ->
+      let
+        cycleLength = i - i0
+        spinsToGo = 1000000000 - i
+        stepsToGo = spinsToGo `mod` cycleLength
+      in
+        case stepsToGo of
+          0 -> pat
+          _ -> snd $ more !! (stepsToGo - 1)
+
+
+-- | Yield every 4th value, starting from the first.
+every4th :: [a] -> [a]
+every4th [] = []
+every4th (x:xs) = x : every4th (drop 3 xs)
+
+-- | Iteratively tilt THEN rotate.  A pre-rotation may be required
+-- to put the pattern in the correct starting state.
+--
+-- The initial state IS included in the output.
+--
+tilts :: Pattern -> [Pattern]
+tilts = iterate (rotr . fmap tiltLeft)
 
 type Pattern = [String]
+
+orient, rotr :: Pattern -> Pattern
+orient = reverse . transpose
+rotr = fmap reverse . transpose
 
 -- | Tilt the row left ("East") and slide them rocks
 tiltLeft :: String -> String
@@ -29,3 +65,7 @@ loadLeft =
   . fmap (\(i,c) -> case c of 'O' -> i ; _ -> 0)
   . zip [1..]
   . reverse
+
+-- | Extract list of row-wise load values
+extractLoads :: Pattern -> [Int]
+extractLoads = fmap loadLeft
