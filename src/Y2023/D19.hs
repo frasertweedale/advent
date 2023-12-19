c9f3fdd34dcf61c6d56696080135df409cfcbb4d
Fraser Tweedale <frase@frase.id.au>
2023-12-19 16:23:25 +1000

    2023 day 19 puzzle 2
    Time taken: 35 minutes
    Topics: combinatorics

--- src/Y2023/D19.hs
+++ src/Y2023/D19.hs
@@ -9,7 +9,7 @@ import qualified Util.Map as Map
 import Util.Parser
 
 solutions :: [IO ()]
-solutions = [s1 go1]
+solutions = [s1 go1, s1 go2]
 
 s1 :: (Show a) => (Input -> a) -> IO ()
 s1 go =
@@ -38,19 +38,86 @@ matchRule part = \case
     | otherwise           -> matchRule part more
 
 matches :: Part -> Test -> Bool
-matches part (Test cat op n) = getOp op (getCategory part cat) n
+matches part (Test cat op n) = getOp op (getCategory cat part) n
 
-getCategory :: Part -> Category -> Int
-getCategory (x,m,a,s) = \case
+getCategory :: Category -> (a,a,a,a) -> a
+getCategory cat (x,m,a,s) = case cat of
   X -> x
   M -> m
   A -> a
   S -> s
 
+putCategory :: Category -> a -> (a,a,a,a) -> (a,a,a,a)
+putCategory cat z (x,m,a,s) = case cat of
+  X -> (z,m,a,s)
+  M -> (x,z,a,s)
+  A -> (x,m,z,s)
+  S -> (x,m,a,z)
+
 getOp :: Op -> (Int -> Int -> Bool)
 getOp = \case
   GreaterThan -> (>)
   LessThan -> (<)
+
+
+type Range = (Int,Int)
+type Ranges = (Range,Range,Range,Range)
+
+go2 :: Input -> Int
+go2 (workflowMap, _) =
+  sum
+  $ fmap prodRanges
+  $ filter rangesValid
+  $ processWorkflow "in" startRanges
+  where
+    startRange = (1,4000)
+    startRanges = (startRange, startRange, startRange, startRange)
+
+    processWorkflow :: String -> Ranges -> [Ranges]
+    processWorkflow k ranges = case Map.lookup k workflowMap of
+      Nothing -> error $ "unknown workflow: " <> k
+      Just rules -> processRules ranges rules
+
+    processRules :: Ranges -> [Rule] -> [Ranges]
+    processRules ranges = \case
+      []                      -> error "rules exhausted"
+      ((Nothing, r):_)        -> handleResult ranges r
+      ((Just test, r):more)   ->
+        let
+          (yeah,nah) = splitRange test ranges
+        in
+          handleResult yeah r
+          <> processRules nah more
+
+    handleResult :: Ranges -> Either String Result -> [Ranges]
+    handleResult ranges = \case
+      Left k        -> processWorkflow k ranges
+      Right Accept  -> [ranges]
+      Right Reject  -> []
+
+    prodRanges :: Ranges -> Int
+    prodRanges (x,m,a,s) =
+      product $ fmap (\(lo,hi) -> hi - lo + 1) [x,m,a,s]
+
+rangesValid :: Ranges -> Bool
+rangesValid (x,m,a,s) = all rangeValid [x,m,a,s]
+
+rangeValid :: Range -> Bool
+rangeValid (lo,hi) = lo <= hi
+
+-- | Split ranges into those satisfying the test, and those not
+splitRange :: Test -> Ranges -> (Ranges, Ranges)
+splitRange (Test cat op n) ranges =
+  let
+    (lo,hi) = getCategory cat ranges
+  in
+    case op of
+      GreaterThan ->
+        ( putCategory cat (n + 1,hi) ranges   -- satisfies
+        , putCategory cat (lo,n)     ranges ) -- does not satisfy
+      LessThan    ->
+        ( putCategory cat (lo,n - 1) ranges   -- satisfies
+        , putCategory cat (n,hi)     ranges ) -- does not satisfy
 
 
 type Input = (Map.Map String [Rule], [Part])
